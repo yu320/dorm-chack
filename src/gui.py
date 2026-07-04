a292262f64888634c87e238a76833b6c4c7b10fe
Youzih <acn940505@gmail.com>
2026-07-04 16:12:51 +0800

    Bump version to v1.8.5

--- src/gui.py
+++ src/gui.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from src.config import load_settings, save_settings
 from src.processor import BatchProcessor
 
-APP_VERSION = "v1.8.4"
+APP_VERSION = "v1.8.5"
 
 class OCRDesktopApp(ctk.CTk):
     def __init__(self):
